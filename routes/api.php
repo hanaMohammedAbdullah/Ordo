c8f46a9365bf2dfcc49b5bbfea7064c1113b05ee
Hasan Sabah <hasansabah786@gmail.com>
2023-04-23 10:02:58 +0300

    changes to route and someother files

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\FoodDetailController;
+
+use App\Http\Controllers\Admin\FoodController as AdminFoodController;
+use App\Http\Controllers\Admin\OrderController as AdminOrderController;
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\DeskController as AdminDeskController;
+use App\Http\Controllers\Admin\SubCategoryController as AdminSubCategoryController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +27,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('qr-login', [AuthController::class, 'qrLogin']);
+
+Route::middleware(['accessToken'])->group(function () {
+
+    Route::get('menu/categories', [MenuController::class, 'index']);
+    Route::get('menu/categories/{category}', [MenuController::class, 'showFoodsBelongToCategory']);
+    Route::get('menu/sub-categories/{subcategory}', [MenuController::class, 'showFoodsBelongToSubCategory']);
+
+    Route::get('menu/food-details/{food}', [FoodDetailController::class, 'show']);
+
+    Route::post('carts', [CartController::class, 'index']);
+    Route::post('carts/{food}', [CartController::class, 'store']);
+
+    Route::get('orders', [OrderController::class, 'index']);
+    Route::post('orders', [OrderController::class, 'store']);
+    Route::delete('orders/{cart}', [OrderController::class, 'destroy']);
+
+    Route::post('feedback/foods/{food}', [FeedbackController::class, 'store']);
+});
+
+Route::middleware(['admin'])->prefix('admin')->group(function () {
+
+    Route::resource('categories', AdminCategoryController::class)->except(['create', 'show', 'edit']);
+    Route::resource('sub-categories', AdminSubCategoryController::class)->except(['create', 'show', 'edit']);
+    Route::resource('foods', AdminFoodController::class)->except(['create', 'show', 'edit']);
+    Route::resource('desks', AdminDeskController::class)->except(['create', 'show', 'edit']);
+    Route::resource('orders', AdminOrderController::class)->except(['create', 'store', 'show', 'edit', 'destroy']);
 });
