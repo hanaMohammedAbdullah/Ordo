baa243edca6a7d4436032d0e49cbb324fadce461
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2023-04-24 02:05:22 +0300

    adding about page and maleinga 404 router

--- react/src/App.js
+++ react/src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Lunch from "./pages/Lunch";
+import About from "./pages/About";
+import Error from "./pages/Error";
 function App() {
   return (
     <div cl>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/lunch" element={<Lunch/>} />
+        <Route path="/lunch" element={<Lunch />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
     </div>
   );
---
+++ react/src/pages/Error.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../Layouts/Layout";
+
+export default function Home() {
+    return (
+        <Layout>
+            <div className="grid grid-cols-2 ">404 Error</div>
+        </Layout>
+    );
+}
